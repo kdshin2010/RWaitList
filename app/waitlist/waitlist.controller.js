e0dd1ebd154204a087f8dbfdca3b9d1fc98d1b5f
kdshin2010 <kdshin2010@gmail.com>
2016-10-29 21:26:18 -0700

    connected app to fire base In the controller we made a connection to firebase setting    var fireParties = firebase.database().ref(parties);    connect data

--- app/waitlist/waitlist.controller.js
+++ app/waitlist/waitlist.controller.js
@@ -3,16 +3,21 @@
 		.module('app.waitlist')
 		.controller('WaitListController', WaitListController)
 
-	// WaitListController.$inject = []
+	WaitListController.$inject = ['$firebaseArray']
 
-	function WaitListController() {
+	function WaitListController($firebaseArray) {
 		console.log('here')
 		var vm = this;
 		vm.addParty = addParty
 
-		vm.parties = [1,2,4];
+		//https://waitlist-ed21f.firebaseio.com
+
+		var fireParties = firebase.database().ref('parties');
+		vm.parties = $firebaseArray(fireParties);
+
 		function addParty() {
-			vm.parties.push('another');
+			//use firebase method
+			vm.parties.$add('another');
 		}
 
 		//define add function party
