9b9f14fb3b4231fd0eaf3e512aef4107c1ff96e3
kdshin2010 <kdshin2010@gmail.com>
2016-10-29 21:01:42 -0700

    Used add the add party functionality used ng-submit in the HTML form to add party

--- app/waitlist/waitlist.controller.js
+++ app/waitlist/waitlist.controller.js
@@ -8,8 +8,14 @@
 	function WaitListController() {
 		console.log('here')
 		var vm = this;
+		vm.addParty = addParty
 
-		vm.parties = [1,2,4]
+		vm.parties = [1,2,4];
+		function addParty() {
+			vm.parties.push('another');
+		}
+
+		//define add function party
 	}
 
 })()
